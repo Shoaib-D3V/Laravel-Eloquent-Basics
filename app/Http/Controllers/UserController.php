ebca49cac7ff685408c3b16dc673ca6410738fab
Shoaib Qureshi <shoaib@d3vtech.com>
2024-11-14 13:56:13 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -15,14 +17,19 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+       // replace this with Eloquent statemen0
+         $users = User::whereNotNull('email_verified_at')
+                ->orderBy('created_at', 'desc')
+                ->limit(3)
+                ->get(); // replace this with Eloquent statement
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+      //  $user = NULL; // TASK: find user by $userId or show "404 not found" page
+         $user = User::findOrFail($userId);
 
         return view('users.show', compact('user'));
     }
@@ -31,7 +38,12 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+       $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email,
+        ],[
+            'password' => Hash::make(Str::random(10)),
+        ]);
 
         return view('users.show', compact('user'));
     }
@@ -40,7 +52,13 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        // updated or created user
+         $user = User::updateOrCreate([
+            'name' => $name,
+        ],[
+            'email' => $email,
+            'password' => Hash::make(Str::random(10)),
+        ]);
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +70,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+         User::destroy($request->users);
 
         return redirect('/')->with('success', 'Users deleted');
     }
@@ -60,7 +79,7 @@ class UserController extends Controller
     {
         // TASK: That "active()" doesn't exist at the moment.
         //   Create this scope to filter "where email_verified_at is not null"
-        $users = User::active()->get();
+          $users = User::whereNotNull('email_verified_at')->get();;
 
         return view('users.index', compact('users'));
     }
